a582dee02c28f620013397d1c5219e01e6ab126f
IshaMandaviya <isha@solulab.com>
2022-04-19 12:49:40 +0530

    data types(tupple,array)

---
+++ data_type/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+    let a = [1, 2, 3, 4, 5];
+  
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+                 "August", "September", "October", "November", "December"];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // This is the same as writing let a = [3, 3, 3, 3, 3];
+    let a = [3; 5];
+
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+    
+    
+
+}
