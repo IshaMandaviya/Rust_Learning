c25d2efd4d32bf82dbe94fb000d612573c207132
IshaMandaviya <isha@solulab.com>
2022-05-06 22:55:37 +0530

    control flow practice

--- Learning_rust/src/main.rs
+++ Learning_rust/src/main.rs
@@ -1,10 +1,32 @@
-fn main() {
-    let a = 20;
-    let b = 20;
-    let c = add(a, b);
-    println!("Hello, world!{}", c);
-}
+// // function
+// fn main() {
+//     let a = 20;
+//     let b = 20;
+//     let c = add(a, b);
+//     println!("Hello, world!{}", c);
+// }
 
-fn add(a: i32, b: i32) -> i32 {
-    return a + 10;
+// fn add(a: i32, b: i32) -> i32 {
+//     return a + 10;
+// }
+
+// conditional flow
+fn main() {
+    let a = 10;
+    let b = 120;
+    let c = 30;
+    if a > b {
+        if a > c {
+            println!("a is big nomber");
+        } else {
+            println!("c is big number");
+        }
+    } else {
+        if b > c {
+            println!("B is big number");
+        }
+        else{
+            println!("c is big number");
+        }
+    }
 }
